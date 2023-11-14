f018ca09d355f0f81f4efc661886139cec191f5f
qustoo <zalevskiy@sfedu.ru>
2023-11-14 23:30:18 +0300

    initial migration

--- app/hotels/models.py
+++ app/hotels/models.py
@@ -5,8 +5,8 @@ from sqlalchemy import Column,Integer,String,JSON
 class Hotels(Base):
     __tablename__ = "hotels"
     id = Column(Integer,primary_key=True)
-    name = Column(String)
+    name = Column(String,nullable=False)
     location = Column(String)
     services = Column(JSON)
-    rooms_quantity = Column(Integer)
+    rooms_quantity = Column(Integer,nullable=False)
     image_ig = Column(Integer)
