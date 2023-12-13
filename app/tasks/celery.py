240bde25a0f69ed2ff105100bc47a9138c4d462f
qustoo <zalevskiy@sfedu.ru>
2023-12-14 02:21:44 +0300

    set new task, and set config env values in celery engine

--- app/tasks/celery.py
+++ app/tasks/celery.py
@@ -1,4 +1,8 @@
 from celery import Celery
+from app.config import settings
 
-
-celery = Celery("tasks", broker="redis://localhost", include=["app.tasks.task"])
+celery = Celery(
+    "tasks",
+    broker=f"redis://{settings.REDIS_HOST}:{settings.REDIS_PORT}",
+    include=["app.tasks.tasks"],
+)
--- app/tasks/task.py
+++ app/tasks/task.py
@@ -1,9 +1,13 @@
+from pydantic import EmailStr
+from app.config import settings
+from app.tasks.email_tasks import create_booking_confirmation_template
 from app.tasks.celery import celery
 from PIL import Image
 from pathlib import Path
+import smtplib
 
 
-# @celery.task
+@celery.task
 def process_pic(
     path: str,
 ):
@@ -13,3 +17,12 @@ def process_pic(
     im_resized_200_100 = im.resize((200, 100))
     im_resized_1000_500.save(f"app/static/images/resized_1000_500_{im_path.name}")
     im_resized_200_100.save(f"app/static/images/resized_1000_500_{im_path.name}")
+
+
+@celery.task
+def send_booking_confirmation(booking: dict, email_to: EmailStr):
+    email_to_mock = settings.SMTP_USER
+    msg_content = create_booking_confirmation_template(booking, email_to)
+    with smtplib.SMTP_SSL(settings.SMTP_HOST, settings.SMTP_PORT) as server:
+        server.login(settings.SMTP_USER, settings.SMTP_PASS)
+        server.send_message(msg_content)
